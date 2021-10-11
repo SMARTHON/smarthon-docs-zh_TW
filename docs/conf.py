336fa71a01104762d788a9ff2aa544a906ffa7f9
samlcy1997 <samlcy1997@gmail.com>
2021-10-11 19:19:28 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+language="zh_TW"
 
 # -- Options for HTML output -------------------------------------------------
 
